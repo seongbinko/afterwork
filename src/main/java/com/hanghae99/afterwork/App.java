a649c511ec4e1af2ce377924149e5450f286a1f7
seongbinko <wnrhd1082@gmail.com>
2021-04-26 21:56:05 +0900

    build : spring-security, application.yml

--- src/main/java/com/hanghae99/afterwork/App.java
+++ src/main/java/com/hanghae99/afterwork/App.java
@@ -1,13 +1,17 @@
 package com.hanghae99.afterwork;
 
+import com.hanghae99.afterwork.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+@EnableJpaAuditing
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
 }
