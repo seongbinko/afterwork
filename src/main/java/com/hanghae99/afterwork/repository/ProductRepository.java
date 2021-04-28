9886766dc4e4ee8ca1a1f476cc211487fdcff27c
NamSeok_DT <skatjr30@naver.com>
2021-04-28 21:52:44 +0900

    feat : Add Crawling HobyintheboxCategory

--- src/main/java/com/hanghae99/afterwork/repository/ProductRepository.java
+++ src/main/java/com/hanghae99/afterwork/repository/ProductRepository.java
@@ -5,9 +5,7 @@ import com.hanghae99.afterwork.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByCategory(Category category, Pageable pageable);
 }
