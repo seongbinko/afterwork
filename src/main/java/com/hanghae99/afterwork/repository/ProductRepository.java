aced2de4e1ebb823bc7a743f0bf3ce800a72f607
27조 <79817453+unkwn22@users.noreply.github.com>
2021-04-29 23:08:05 +0900

    chores: recover application setting to develop setting, delete repository unuse import

--- src/main/java/com/hanghae99/afterwork/repository/ProductRepository.java
+++ src/main/java/com/hanghae99/afterwork/repository/ProductRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByCategory(Category category, Pageable pageable);
     boolean existsByProductId(Long productId);
