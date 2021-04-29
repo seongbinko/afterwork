44a24fa3ca13da901359fd5a07472881022d1e18
NamSeok_DT <skatjr30@naver.com>
2021-04-29 20:17:05 +0900

    feat : Create /api/user POST, DELETE

--- src/main/java/com/hanghae99/afterwork/repository/CategoryRepository.java
+++ src/main/java/com/hanghae99/afterwork/repository/CategoryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Optional<Category> findById(Long id);
+    Optional<Category> findByCategoryId(Long categoryId);
     Optional<Category> findByName(String name);
 
 }
