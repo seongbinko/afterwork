4f9b5438d2b602c83490c211c3b5923fc4c3ddb9
NamSeok_DT <skatjr30@naver.com>
2021-05-03 23:06:55 +0900

    chore : ProductRepository @Query 추가 // 중복검색 방지

--- src/main/java/com/hanghae99/afterwork/repository/ProductRepository.java
+++ src/main/java/com/hanghae99/afterwork/repository/ProductRepository.java
@@ -5,12 +5,16 @@ import com.hanghae99.afterwork.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByCategory(Category category, Pageable pageable);
-    Page<Product> findAllByTitleLike(String keyword, Pageable pageable);
+
+    @Query(value = "select * from products where title like :keyword group by title",nativeQuery = true)
+    Page<Product> findAllByTitleLike(@Param("keyword") String keyword, Pageable pageable);
     boolean existsByProductId(Long productId);
     Product findByProductId(Long productId);
 }
