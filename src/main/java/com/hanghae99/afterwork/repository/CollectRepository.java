7d1106e7ae857a031c4308dc116dc45ed26a0264
27조 <79817453+unkwn22@users.noreply.github.com>
2021-05-05 21:58:08 +0900

    chore: change in api collect result, collect repository remove annotation

--- src/main/java/com/hanghae99/afterwork/repository/CollectRepository.java
+++ src/main/java/com/hanghae99/afterwork/repository/CollectRepository.java
@@ -4,12 +4,9 @@ import com.hanghae99.afterwork.model.Collect;
 import com.hanghae99.afterwork.model.Product;
 import com.hanghae99.afterwork.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
-@Transactional(readOnly = true)
 public interface CollectRepository extends JpaRepository<Collect, Long> {
     void deleteAllByUser(User user);
     void deleteByCollectId(Long collectId);
